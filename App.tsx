ca6b0cb3fed3aaaf8c78145018c4826bcefa983a
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-25 18:57:53 -0300

    update page App

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
@@ -13,11 +14,19 @@ export default function App() {
           <Text style={styles.numberContainer}>0</Text>
         </View>
       </View>
-      <View>
-        <Text>
-          Você ainda não tem tarefas cadastradas Crie tarefas e organize seus
-          itens a fazer
-        </Text>
+      <View style={styles.containerThree}>
+        <View>
+          <Text style={styles.textThree}>
+            Você ainda não tem tarefas cadastradas
+          </Text>
+          <Text style={styles.textFour}>
+            Crie tarefas e organize seus itens a fazer
+          </Text>
+        </View>
+        <ImageBackground
+          source={require("./assets/image.png")}
+          style={{ width: 56, height: 56 }}
+        />
       </View>
     </View>
   );
@@ -34,10 +43,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#333333",
   },
   containerTwo: {
     flexDirection: "row",
     gap: 8,
+  },
+  containerThree: {
+    width: 327,
+    height: 208,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 16,
   },
   textOne: {
     color: "#4EA8DE",
@@ -50,6 +68,18 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 14,
     lineHeight: 17,
+  },
+  textThree: {
+    color: "#808080",
+    fontWeight: "700",
+    fontSize: 14,
+    textAlign: "center",
+  },
+  textFour: {
+    color: "#808080",
+    fontWeight: "400",
+    fontSize: 14,
+    textAlign: "center",
   },
   numberContainer: {
     backgroundColor: "#333333",
